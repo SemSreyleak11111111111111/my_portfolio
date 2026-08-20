c189875f26237d8616ea0e55430a2c6ae7c97656
SemSreyleak11111111111111 <kanhaleak777@gmail.com>
2026-08-20 11:00:10 +0700

    add logo

--- src/data/logoDesign.js
+++ src/data/logoDesign.js
@@ -12,6 +12,12 @@ const logoDesign = [
     image:
       "https://i.pinimg.com/736x/8d/78/47/8d7847032b725070a5063b145cf79c63.jpg",
   },
+   {
+    id: 3,
+    title: " Logo",
+    image:
+      "https://i.pinimg.com/736x/7e/96/0d/7e960d1712a75546de518f26ff602b98.jpg",
+  },
 ];
 
 export default logoDesign;
